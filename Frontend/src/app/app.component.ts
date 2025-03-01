ee10098396ba1159899bbff5962d84cba175f5ed
Yul Cardenas <69341405+jamesyul@users.noreply.github.com>
2025-03-01 23:44:06 +0100

    update hero

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component'; // Ajusta la ruta si es diferente
+import { HeroComponent } from './hero/hero.component';       // Ajusta la ruta si es diferente
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  //imports: [RouterOutlet],
+  standalone: true,
+  imports: [HeaderComponent, HeroComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
